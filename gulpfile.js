6eec296b9b89faeb2b310fa890105f144c63f075
shruti Khandekar <katdareshruti@yahoo.com>
2019-09-28 20:47:44 -0700

    Added new mixins
    Added mixins for dimensions, float and clearfix.
    Moved media queried to media partial.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 
 var {src, dest, watch} = require('gulp');
 var sass = require('gulp-sass');
-var csso = require('gulp-csso');
+//var csso = require('gulp-csso');
 sass.compiler = require('node-sass');
 
 function css(){
     return src('src/*.scss')
     .pipe(sass())
-    .pipe(csso())
+    
     .pipe(dest('dist/css'))
 };
 
@@ -15,3 +15,5 @@ exports.default = function(){
     watch('src/*.scss', css);
     watch('src/modules/*.scss', css);
 };
+
+//.pipe(csso())
